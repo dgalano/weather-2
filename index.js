c6d07d7226f3168811415056453ea2422ba4aa47
dgalano <55563056+dgalano@users.noreply.github.com>
2022-02-27 21:36:11 -1000

    Update index.js

--- index.js
+++ index.js
@@ -15,22 +15,28 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+    // change zip to city
+        var city = String(req.body.cityInput);
+        console.log(req.body.cityInput);
     
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
+    //Notes - I don't know what I did wrong at first, but after putting in my own API key, the program stopped working properly. This is my 5th attempt. Suddenly it started to work again. Don't know what I did differently, but it's concerning to me.
+    // The answer lies here, api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    // I deleted the &units and got a rediculous number in the 100s. I wanted to keep to the answer as close as possible, but got very far away instead. I tested out the temp and compared it to what google had. What google gave was off by two, but it's close enough, I suppose.
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "bac32555bf43c6ce68ccbd1136960998";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
         console.log(response.statusCode);
         
         // gets individual items from Open Weather API
+        // add in both humidity and wind speed
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+            const humid = weatherData.main.humidity;
+            const wind = weatherData.wind.speed;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
@@ -38,7 +44,7 @@ app.post("/", function(req, res) {
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit. In terms of humidity, it is " + humid + " humid. Winds speeds are going " + wind + " fast. <h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
@@ -50,3 +56,5 @@ app.post("/", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
 console.log ("Server is running on port")
 });
+
+//homwork - change from ZIP to city
